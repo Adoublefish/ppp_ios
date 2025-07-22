761da5621a47a797328fe1936b76dc366a473b2a
Yu Fu <yufu6@asu.edu>
2025-07-22 11:28:53 -0700

    Initial Commit

---
+++ ppp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ppp
+//
+//  Created by 傅煜 on 7/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ppp/pppApp.swift
@@ -0,0 +1,17 @@
+//
+//  pppApp.swift
+//  ppp
+//
+//  Created by 傅煜 on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct pppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
